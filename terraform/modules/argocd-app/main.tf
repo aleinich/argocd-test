0d72b731125015e8770f8d9fc012534510bd367d
aleinich <aleinich@yahoo.com>
2026-02-22 11:20:48 +0100

    Update main.tf

--- terraform/modules/argocd-app/main.tf
+++ terraform/modules/argocd-app/main.tf
@@ -1,14 +1,33 @@
-provider "argocd" {
-  server_addr = "localhost:8080" # Dopo aver fatto: kubectl port-forward svc/argocd-server -n argocd 8080:443
-  insecure    = true
-  plain_text  = true
-}
+  metadata {
+    name      = var.app_name
+    namespace = "argocd"
+  }
 
-module "victoria_metrics_umbrella" {
-  source          = "./modules/argocd_app"
-  app_name        = "victoria-metrics-ha"
-  repo_url        = "https://github.com/aleinich/argocd-test.git"
-  target_revision = "HEAD"
-  chart_path      = "." # Il Chart.yaml è nella root del repo
-  namespace       = "victoria-metrics"
+  spec {
+    project = "default"
+
+    source {
+      repo_url        = var.repo_url
+      target_revision = var.target_revision
+      path            = var.chart_path # Punto dove si trova il Chart.yaml (root ".")
+
+      helm {
+        # Carica il values.yaml che definisce node1, node2 e vmauth
+        value_files = ["values.yaml"]
+      }
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = var.namespace
+    }
+
+    sync_policy {
+      automated {
+        prune     = true
+        self_heal = true
+      }
+      sync_options = ["CreateNamespace=true"]
+    }
+  }
 }
