1a5581b8b78eee191afa96edd9c63e037b542af0
aleinich <aleinich@yahoo.com>
2026-02-22 15:09:14 +0100

    Create variables.tf

---
+++ terraform/modules/argocd-app/variables.tf
@@ -0,0 +1,19 @@
+variable "app_name" {
+  type = string
+}
+
+variable "repo_url" {
+  type = string
+}
+
+variable "target_revision" {
+  type = string
+}
+
+variable "chart_path" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
