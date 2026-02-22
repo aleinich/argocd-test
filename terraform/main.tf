1db4382bb46e68acdf17869e077e152c26e9626c
aleinich <aleinich@yahoo.com>
2026-02-22 11:20:22 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "argocd" {
+  server_addr = "localhost:8080" # Dopo aver fatto: kubectl port-forward svc/argocd-server -n argocd 8080:443
+  insecure    = true
+  plain_text  = true
+}
+
+module "victoria_metrics_umbrella" {
+  source          = "./modules/argocd_app"
+  app_name        = "victoria-metrics-ha"
+  repo_url        = "https://github.com/aleinich/argocd-test.git"
+  target_revision = "HEAD"
+  chart_path      = "." # Il Chart.yaml è nella root del repo
+  namespace       = "victoria-metrics"
+}
